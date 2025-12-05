f97b89295e4529d15af76a4317a5e1796a6c11a7
Vinayak Tyagi <vinayaktyagi.ed@gmail.com>
2025-12-05 23:00:03 +0530

    feat:extract text from db

--- devlog/__main__.py
+++ devlog/__main__.py
@@ -15,9 +15,10 @@ def init_db():
     c.execute("""
         CREATE TABLE IF NOT EXISTS notes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            message TEXT NOT NULL,
+            raw_text TEXT NOT NULL,
+            summary TEXT NULL,
+            status TEXT NOT NULL default "raw",
             created_at TEXT NOT NULL
-            tag TEXT NOT NULL
         );
     """)
     conn.commit()
@@ -31,20 +32,47 @@ def cli():
 
 
 @cli.command()
-@click.option('--tag', default=None, help="Filter by tag")
 @click.argument("message")
 def add(message):
     """Add a new log message"""
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute("""
-        INSERT INTO notes (message, created_at, tag)
-        VALUES (?, ?, ?);
-    """, (message, datetime.now().isoformat(), tag))
+        INSERT INTO notes (raw_text, created_at)
+        VALUES (?, ?)
+    """, (message, datetime.now().isoformat()))
     conn.commit()
     conn.close()
 
     print(f"[bold green]Added log message:[/] {message}")
+
+@cli.command()
+def process():
+    """Process the log messages"""
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("""
+        select id, raw_text from notes where status = "raw"
+    """)
+    raw_logs = c.fetchall()
+    conn.close()
+
+
+    count = 0
+    for note_id, raw_text in raw_logs:
+        summary = call_llm(raw_text)
+        conn_summary = sqlite3.connect(DB_PATH)
+        c_summary = conn_summary.cursor()
+        c_summary.execute("""
+            UPDATE notes SET summary = ?, status = "summarized" WHERE id = ?
+        """, (summary, note_id))
+        conn_summary.commit()
+        conn_summary.close()
+        count += 1
+
+    print(f"[bold green]Processed {count} notes.[/]")
+
+
 
 
 if __name__ == "__main__":
